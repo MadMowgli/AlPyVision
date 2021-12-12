1a87651192e9693b3d2495e4714f6280a4f01ed5
MadMowgli <laeubin.j@gmail.com>
2021-12-12 13:28:26 +0100

    Moved UnitTest package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 # Let setuptools work
 setuptools.setup(
@@ -17,13 +17,17 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     keywords=['albion', 'opencv', 'computer vision'],
     url='https://github.com/MadMowgli/AlPyVision',
-    package_dir={'': 'AlPyVision'},
     packages=setuptools.find_packages(),
     install_requires=['opencv-python', 'numpy'],
     license='MIT',
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Topic :: Multimedia :: Graphics :: Capture :: Screen Capture',
         'Programming Language :: Python :: 3',
         'Operating System :: Microsoft :: Windows'
     ]
