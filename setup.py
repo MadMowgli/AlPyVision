39971ac47ddb6dcb9488bc3dd5c2d4e42a1a54f3
MadMowgli <laeubin.j@gmail.com>
2021-12-12 12:01:10 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,15 @@ setuptools.setup(
     description='AlPyVision is a Python wrapper to work with openCV and the Albion Online game client.',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    keywords=['albion', 'opencv', 'computer vision'],
     url='https://github.com/MadMowgli/AlPyVision',
     packages=setuptools.find_packages(),
     install_requires=['opencv-python', 'numpy'],
-    license='MIT'
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows'
+    ]
 )
 
